eb211945d884689b958cd8c969cac67a89663ce2
wick786786 <thesatyam786786@gmail.com>
2024-04-10 19:28:44 +0530

    new changes

--- index.php
+++ index.php
@@ -7,10 +7,21 @@
 </head>
 <body>
 
- <p> 
-    Hello world
+  <h1>Calculator</h1>
 
- </p>
+  <input name="num01" type="number>" placeholder="enter your first number"></input>
+  <br>
+  <select name="operator">
+     <option value="+">Addition (+)</option>
+     <option value="-">subtraction (-)</option>
+     <option value="+">Multiply(*)</option>
+     <option value="+">Divison (/)</option>
+     
+ </select>
+ <br>
+ <input name="num02" type="number>" placeholder="enter your second number"></input>
+ <br>
+ <button >result</button>
     
 </body>
 </html>
