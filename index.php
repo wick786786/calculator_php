5c89c1cca5f6d8f40e5d89d54a04fe0458ccd72b
wick786786 <thesatyam786786@gmail.com>
2024-04-10 20:04:20 +0530

    completed

--- index.php
+++ index.php
@@ -9,19 +9,64 @@
 
   <h1>Calculator</h1>
 
-  <input name="num01" type="number>" placeholder="enter your first number"></input>
+  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+
+  <input name="num01" type="number" placeholder="enter your first number"></input>
   <br>
   <select name="operator">
      <option value="+">Addition (+)</option>
-     <option value="-">subtraction (-)</option>
-     <option value="+">Multiply(*)</option>
-     <option value="+">Divison (/)</option>
-     
+     <option value="-">Subtraction (-)</option>
+     <option value="*">Multiplication (*)</option>
+     <option value="/">Division (/)</option>
  </select>
  <br>
- <input name="num02" type="number>" placeholder="enter your second number"></input>
+ <input name="num02" type="number" placeholder="enter your second number"></input>
  <br>
- <button >result</button>
-    
+ <button type="submit" name="submit">Calculate</button>
+</form>
+
+<?php
+if(isset($_POST['submit'])) {
+    $num01 = htmlspecialchars($_POST["num01"]);
+    $num02 = htmlspecialchars($_POST["num02"]);
+    $op = htmlspecialchars($_POST["operator"]);
+    $error = false;
+
+    if(empty($num01) || empty($num02) || !isset($op)) {
+        $error = true;  
+    }
+
+    if($error) {
+        echo "Please fill all the fields";
+    } else {
+        if(is_numeric($num01) && is_numeric($num02)) {
+            $result = 0;
+            switch($op) {
+                case '+':
+                    $result = $num01 + $num02;
+                    break;
+                case '-':
+                    $result = $num01 - $num02;
+                    break;
+                case '*':
+                    $result = $num01 * $num02;
+                    break;
+                case '/':
+                    if ($num02 != 0) {
+                        $result = $num01 / $num02;
+                    } else {
+                        echo "Division by zero is not allowed.";
+                    }
+                    break;
+                default:
+                    echo "<p>No operation</p>";
+                    break;
+            }
+            echo "The result is: " . $result;
+        }
+    }
+}
+?>
+
 </body>
 </html>
